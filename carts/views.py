6c3a86550d0cb2698d7d62a36265086ab0171d7a
Fenil POnkiya <fenilponkiya8805@gmail.com>
2024-06-20 00:05:56 +0530

    added add_cart, view_cart, grand total functionality

--- carts/views.py
+++ carts/views.py
@@ -5,8 +5,26 @@ from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
-def cart(request):
-    return render(request,'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = (2*total)/100  # aplicar regra de imposto correta aki
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass  # ignora
+    context = {'total': total,
+               'quantity': quantity,
+               'cart_items': cart_items,
+               'tax': tax,
+               'grand_total': grand_total,
+               }
+    return render(request, 'store/cart.html', context)
 
 def _cart_id(request):  
     cart = request.session.session_key
@@ -25,7 +43,6 @@ def add_cart(request, product_id):
             cart_id=_cart_id(request)
         )
     cart.save()
-    print("hey")
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1 
@@ -37,7 +54,20 @@ def add_cart(request, product_id):
                 cart=cart
             )
         cart_item.save()
-    print("yes")
-    return HttpResponse(cart_item.product)
+    return redirect('cart')
 
+def remove_cart(request, product_id):  
+    product = get_object_or_404(Product, id=product_id)
+    try:
+
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
           
